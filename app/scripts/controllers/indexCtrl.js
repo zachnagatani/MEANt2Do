54cbbbca4c27e5d4f6f1b53e85c04b7371a85c40
Zachariah Nagatani <Zachariah Nagatani>
2016-12-08 19:06:12 -0800

    Feat: Logout Functionality

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -2,11 +2,18 @@
 	'use strict';
 
 	angular.module('meantodo')
-		.controller('indexCtrl', ['$scope', 'authentication', function($scope, authentication) {
+		.controller('indexCtrl', ['$scope', '$state', 'authentication', function($scope, $state, authentication) {
 			var self = this;
 			console.log(authentication.isLoggedIn());
 			self.isLoggedIn = authentication.isLoggedIn();
 			self.currentUser = authentication.currentUser();
+			self.logout = function(event) {
+				// Delete the token
+				authentication.logout();
+				// Reload so auth is run again, and we are taken out of the dashboard
+				$state.reload();
+			};
+
 			$scope.$on('loggedIn', function(event, loggedIn) {
 				console.log(loggedIn);
 				self.isLoggedIn = loggedIn;
